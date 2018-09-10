74fd824de886c76af5ded5c33f069896a58c3942
Tanakh <ryuta12281028@i.softbank.jp>
2018-09-10 16:57:44 +0900

    カメラロールの呼び出し

--- bubbleapp/App.js
+++ bubbleapp/App.js
@@ -1,120 +1,52 @@
 import React from 'react';
-import { createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
-import { Animated, AppRegistry, StyleSheet, Text, View, Image, Button, ImageBackground } from 'react-native';
-const Myon = './jpeg/Myon.jpg';
+import { StyleSheet, View, Button, Image } from 'react-native';
+import { ImagePicker, Permissions } from 'expo';
 
-class HomeScreen extends React.Component {
-  render() {
-     return (
-    <View style={styles.container}>
-    <View style={styles.button}>
-      <View style={styles.newbutton}>
-      
-      </View>
-      <View style={styles.space}></View>
-      <View style={styles.optbutton}>
-           <Button
-       title="詳細ページへ"
-       onPress={() => this.props.navigation.navigate('Details')}
-     />
-      </View>
-    </View>
-    <View style={styles.bubble}>  
-      <View style={styles.table}>
-        <ImageBackground source={require (Myon)} style={styles.ImgMyon}>  
-          <Text style={styles.samples}>SAMPLE</Text>
-        </ImageBackground>  
-      </View>
-    </View>
-  </View>
-    );
+export default class App extends React.Component {
+
+  state = {
+    hasCameraRollPermission: null,
+    photo: null
+  };
+
+  async componentWillMount() {
+    // カメラロールに対するPermissionを許可
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    this.setState({ hasCameraRollPermission: status === 'granted' });
   }
-}
 
-class DetailsScreen extends React.Component {
+
   render() {
+    let { hasCameraRollPermission, photo } = this.state;
+    console.log(this.state);
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>詳細ページ</Text>
+      <View style={styles.container}>
+        {hasCameraRollPermission && photo && <Image style={styles.image} source={{ uri: photo.uri }} />}
+        <Button style={styles.button} title={"open Image Picker."} onPress={async () => {
+          // Image Pickerを起動する
+          let result = await ImagePicker.launchImageLibraryAsync();
+          console.log(result);
+          this.setState({ photo: result });
+        }} />
       </View>
     );
   }
 }
 
-const RootStack = createStackNavigator(
-  {
-    Home: {
-      screen: HomeScreen,
-    },
-    Details: {
-      screen: DetailsScreen,
-    },
-  },
-  {
-    initialRouteName: 'Home',
-  }
-);
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'column',
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: '#fff',
+    alignItems: 'center',
     justifyContent: 'center',
   },
-  button: {
-    marginTop: 20,
-    flex: 0.75,
-    backgroundColor: 'white',
-    flexDirection: 'row'
-  },
-  newbutton: {
-    flex: 1,
-    backgroundColor: 'pink',
-  },
-  space: {
-    flex: 2,
-  },
-  optbutton: {
-    flex: 1,
-    backgroundColor: 'pink',
-  },
-  bubble: {
-    flex: 9,
-    backgroundColor: 'red',
-  },
-  table: {
-    flex: 1,
+  image: {
     width: '100%',
-    backgroundColor: '#30603080',
+    height: '100%',
+    flex: 9,
   },
-  nodemin:{
-    flex: 1,
-    backgroundColor: '#30306080',
-  },
-  nodemid: {
-    flex: 1,
-    backgroundColor: '#60303080',
-  },
-  nodemax: {
-    flex: 1,
-  },
-  ImgMeiling: {
-    flex: 1,  
-  },
-  ImgMyon:{
-    flex: 1,
-  },
-  samples: {
-    textAlign: 'center',
-    fontSize: 150,
-    color: 'red'
-  },
-});
-
-export default class App extends React.Component {
-  render() {
-    return <RootStack />;
+  button: {
+    flex:1
   }
-}
-
+});
