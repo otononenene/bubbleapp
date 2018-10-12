d0139792ae2fc8ee4c3ae7b80550b36f6f15f9c8
Tanakh <ryuta12281028@i.softbank.jp>
2018-10-13 03:14:15 +0900

    add weather component

---
+++ bubbleapp/bubbleapp/components/calendar/calendar.js
@@ -0,0 +1,19 @@
+import {} from 'react-native-calendars';
+import React from 'react';
+import {Text, View} from 'react-native'
+
+export class Calendar extends React.Component{
+    render(){
+        return (
+            <View/>
+        );
+    }
+}
+
+export class OptCalendar extends React.Component{
+    render() {
+        return (
+          <View/>
+        );
+    }
+}
