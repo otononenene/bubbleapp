8bde8a7567ba0ada55adf7adac3005a57096198a
Tanakh <ryuta12281028@i.softbank.jp>
2018-10-13 11:49:23 +0900

    weather.jsの変更

--- bubbleapp/components/Weather/weather.js
+++ bubbleapp/components/Weather/weather.js
@@ -1,18 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component ,ActivityIndicator} from 'react';
 import {  ScrollView, StyleSheet,Picker } from 'react-native';
-import { Constants } from 'expo';
-// You can import from local files
-import AssetExample from './components/AssetExample';
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-elements'; // 0.18.5
-import { ActivityIndicator, Text, View  } from 'react-native';
-export  class Weather extends Component {
+import {  Text, View  } from 'react-native';
+export  default class Weather extends Component {
 //state = { movies: [] };
 constructor(props) {
     super(props)
     this.state = {placeName: null, weather: null, temperature: null, loading: false}
     //
-    this.Places = [{name: '札幌', id: 2128295}, {name: '東京', id: 1850147},
+    this.Places = [{name:'選択してください',id:''},{name: '札幌', id: 2128295}, {name: '東京', id: 1850147},
                    {name: '大阪', id: 1853909}, {name: '沖縄', id: 1894616},
                   {name:'福岡',id:1863967}]
     this.OpenWeatherMapKey = "a7d4d9333ffb3e2e2ca1affd07dd9277"
@@ -33,31 +28,60 @@ selectPlace(index) {
     .then((json) => {
       delay(700)
       .then(() => this.setState({weather: json.weather[0].description,
-                                 temperature: json.main.temp, loading: false}))
+                                 temperature: json.main.temp, loading: true}))
     })
     .catch((response) => {
       this.setState({loading: false})
       console.log('** error **', response)
     })
   }
+    
+  static navigationOptions = ({ navigation }) => {
+    return {
+       header: () => null
+    } 
+  }
+
   render() {
+    if(this.state.loading){
+      return(
+        <View style={styles.container2}>
+        <Text style={styles.text}>{this.state.placeName}</Text>
+        <Text style={styles.text}>{this.state.weather}</Text>
+        <Text style={styles.text}>{this.state.temperature}</Text>
+        </View>
+      )
+    }
+    else{
     return (
     <View style={styles.container}>
         <Text style={styles.title}>
             {this.state.place ? this.state.place + 'の天気' : '天気情報'}
         </Text>
-        <Text>{this.state.loading?this.state.weather:'null'}</Text>
-        <Text>{this.state.loading?this.state.temperature:'null'}</Text>
         <Picker style={[styles.picker]} 
             itemStyles={styles.pickerItem}
-            onValueChange={(itemValue)=>{selectPlace(itemValue)}  }
-
+            onValueChange={(selectItem)=>{this.selectPlace(selectItem+1)}  }
+            selectedValue={this.Places.Name}
         >
                 {this.Places.map((place, ix) => <Picker.Item key={ix} value={ix} label={place.name} />)}       
-            </Picker>
+        </Picker>
     </View>
         );
+      }
     }
+}
+export class OptWeather extends React.Component{
+
+    render() {
+
+        return (
+
+          <View/>
+
+        );
+
+    }
+
 }
 const styles=StyleSheet.create({
     container:{
@@ -66,6 +90,14 @@ const styles=StyleSheet.create({
         alignItems:'center',
         padding:20,
         backgroundColor:'white'
+    },
+    container2:{
+        flex:1,
+        flexDirection:'column',
+        alignItems:'center',
+        padding:20,
+        backgroundColor:'white',
+        justifyContent:'center'
     },
     title:{
         marginTop:20,
@@ -79,5 +111,9 @@ const styles=StyleSheet.create({
     },
     pickerItem:{
         color:'blue'
+    },
+    text:{
+        color:'blue',
+        fontSize:20
     }
 })
